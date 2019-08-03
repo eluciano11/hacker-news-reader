a286b62b55dc8158de080c580908065c63c9f231
Emmanuel Luciano <emma.luciano11@gmail.com>
2019-08-03 16:05:04 -0400

    Adds service worker for offline functionality

---
+++ src/sw.js
@@ -0,0 +1,27 @@
+self.addEventListener("install", event => event.waitUntil(self.skipWaiting()));
+self.addEventListener("activate", event =>
+  event.waitUntil(self.clients.claim())
+);
+
+// Prefetch javascript bundles, static images and svgs.
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+workbox.precaching.precacheAndRoute([
+  { url: "/yc.png" },
+  { url: "/wifi.svg" },
+  { url: "/no-wifi.svg" }
+]);
+
+// app-shell
+workbox.routing.registerRoute("/", workbox.strategies.networkFirst());
+
+/**
+ * Cache requests that go to the Hacker News API.
+ * The service worker will use the response that comes over the network and
+ * update the cache if it's different.
+ * When the network response is not available it will use the data that
+ * has been stored in the cache instead.
+ */
+workbox.routing.registerRoute(
+  /^https:\/\/hacker-news.firebaseio.com\/.*/,
+  new workbox.strategies.NetworkFirst()
+);
